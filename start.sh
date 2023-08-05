ea58849aeab6c5dd92ded4f24b2ccd746c9c2bd9
IBRAHIM KHAN <119599775+Theprofffesorr@users.noreply.github.com>
2023-08-05 18:26:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Theprofffesorr/Theprofffesorr /aaaaaaaaaa
+  git clone https://github.com/Theprofffesorr/Theprofffesorr /Theprofffesorr
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Theprofffesorr
+fi
 cd /Elsa
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
